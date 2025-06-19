c9fa4fdb5e18782c8399e079b130cf79087fc3ef
JuanMaRios <j.rios2@utp.edu.co>
2025-06-19 08:59:26 -0500

    ya se pueden crear las categorias en la base de datos

--- archivos_backend/categorias.php
+++ archivos_backend/categorias.php
@@ -7,12 +7,39 @@ $categoria = "";
 $categoria_error = "";
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $input_categoria=trim($_POST["categoria"]);
-    if(!empty($input_categoria)){
-        $categoria = $input_categoria;
+    if(!empty($_POST["categoria"])){
+        $categoria = $_POST["categoria"];
     }else{
         $categoria_error = "Campo no valido";
+         header("Location:../index_admin.php");
     }
+
+
+    if(empty($categoria_error)){
+        $_SESSION["errorcategoria"] = "";
+        $sql = "INSERT INTO categorias(nombre) VALUES (?)";
+        if($stmt = mysqli_prepare($conexion,$sql)){
+            mysqli_stmt_bind_param($stmt,"s",$param_categoria);
+
+            //Crear parametros
+            $param_categoria = $categoria;
+
+            if(mysqli_stmt_execute($stmt)){
+                header("Location:../index_admin.php");
+
+                exit();
+            }else{
+                echo "Error al crear categoria";
+            }
+        }
+        //cerramos
+        mysqli_stmt_close($stmt);
+    }
+    //Alertas
+    else{
+        $_SESSION["errorcategoria"]="El campo está vacio";
+    }
+mysqli_close($conexion);
 }
 
 
--- index_admin.php
+++ index_admin.php
@@ -13,7 +13,27 @@ include "includes/sidebar.php"
 <form action="archivos_backend/categorias.php" method="POST">
     <input type="text" name="categoria">
 
-    <input type="submit" name="subir-categoria">
+    <input type="submit" name="subir-categoria" value="crear">
+    <?php if(!empty($_SESSION["errorcategoria"])): ?>
+        <div id="alertacategoria" style="color:red; font-size=0.5rem; font-family:sans-serif; width:150px;">
+        <?php 
+        echo $_SESSION["errorcategoria"];
+        ?>    
+        </div>
+        <script>
+            setTimeout(() => {
+                const alertacategoria=document.getElementById("alertacategoria");
+                if(alertacategoria){
+                    alertacategoria.style.display="none";
+                }
+            }, 4000);
+        </script>
+
+
+        <?php 
+        unset($_SESSION["alertacategoria"]);
+        endif;
+        ?>
 </form>
 
 <?php 
